317229be218cb6a6061e7d9dbd6b357cec5c149a
LoreDema <lorenzo.demattei@gmail.com>
2015-06-02 12:47:22 +0200

    demon

--- community_discovery.py
+++ community_discovery.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import music_analysis as ma
 import operator
+import codecs
+import pickle
 
 
 def print_vectors(artists):
@@ -207,16 +209,61 @@ def community_listening(communities, listening, artists, output_file):
     out_file.close()
 
 
+def read_demon():
+    communities = []
+    with codecs.open('community/communities_DEMON.txt', 'r') as fp:
+        for line in fp:
+            communities.append(eval(line.split('\t')[1]))
+    return communities
+
+
+def girvan_newman(G):
+
+    if len(G.nodes()) == 1:
+        return [G.nodes()]
+
+    def find_best_edge(G0):
+        """
+        Networkx implementation of edge_betweenness
+        returns a dictionary. Make this into a list,
+        sort it and return the edge with highest betweenness.
+        """
+        eb = nx.edge_betweenness_centrality(G0)
+        eb_il = eb.items()
+        eb_il.sort(key=lambda x: x[1], reverse=True)
+        return eb_il[0][0]
+
+    components = nx.connected_component_subgraphs(G)
+
+    while len(components) == 1:
+        G.remove_edge(*find_best_edge(G))
+        components = nx.connected_component_subgraphs(G)
+
+    result = [c.nodes() for c in components]
+
+    for c in components:
+        result.extend(girvan_newman(c))
+
+    return result
+
+
 def main():
     # reads from file and building the LastFm network
     artists, net = ma.read_data()
     listening = ma.get_node_listening(artists)
-    print_vectors(artists)
-    communities = list(nx.k_clique_communities(net, 5))
-    community_statistics(communities, 'k-clique')
 
-    community_listening(communities, listening, artists, 'k-clique')
-
+    '''
+    communities_kcliques = list(nx.k_clique_communities(net, 5))
+    community_statistics(communities_kcliques, 'k-clique')
+    community_listening(communities_kcliques, listening, artists, 'k-clique')
+    '''
+    '''
+    communities_demon = read_demon()
+    community_statistics(communities_demon, 'demon')
+    community_listening(communities_demon, listening, artists, 'demon')
+    '''
+    communities_gn = girvan_newman(net)
+    print communities_gn
 
 if __name__ == '__main__':
     main()
