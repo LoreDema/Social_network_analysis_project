5fbdf2f0c12d1d73b8d803b3ceb6fc01aee2338c
vincenzorizza <vincenzorizza6@gmail.com>
2015-06-09 13:52:37 +0200

    Aggiunta funzione per creare csv di ascolti per artista + grafici

--- local_diffusion.py
+++ local_diffusion.py
@@ -5,13 +5,14 @@ __email__ = 'lorenzo.demattei@gmail.com, ndrmeini@gmail.com, vincenzorizza6@gmai
 import networkx as nx
 import codecs
 import datetime
+from datetime import datetime
+import itertools
+import matplotlib.pyplot as plt
+import music_analysis as ma_func
 import operator
 from operator import itemgetter
 import sys
-import itertools
-from datetime import datetime
 import types
-import matplotlib.pyplot as plt
 
 
 class ArtistInfo:
@@ -39,6 +40,8 @@ def build_tree(list, threshold):
     tmp = []
     net = nx.DiGraph()
 
+    '''
+
     # Exclude multiple listening of same user
     for item in list:
         if item[1] not in added:
@@ -50,6 +53,8 @@ def build_tree(list, threshold):
     list = sorted(list, key=itemgetter(0))
     for user in list:
         print user[0], " - ", user[1]
+
+    '''
 
     # Assign the root to a new list
     newlist = [list[0]]
@@ -68,10 +73,10 @@ def build_tree(list, threshold):
                     # print "Figlio: ", list[i][1]
                     # print "TIME: ", time_diff
                     newlist.append(list[i])
-                    # net.add_edge(list[i][1], list[j][1], weight=time_diff)
+                    net.add_edge(root[1], list[i][1], weight=time_diff)
 
-    print "Final list: "
-    print newlist
+    # print "Final list: "
+    # print newlist
 
     return net
 
@@ -85,16 +90,19 @@ def unicode_to_date(date_posted):
 # List is ascending sorted by played_on value
 def get_listeners(artist):
     listenings = []
-    with codecs.open('data/cleaned_listenings.csv', 'r', 'utf-8') as fp:
+    with codecs.open('listening_analysis/firsts_listening.csv', 'r', 'utf-8') as fp:
+        c = 1
         for line in fp:
+            # print c, ", ",
+            c += 1
             line = line.strip().split(',')
             try:
                 if line[0] not in listenings:
-                    if line[2] in artist:
-                        listenings.append([unicode_to_date(line[4]), line[0], (line[2])])
+                    if line[1] in artist:
+                        listenings.append([unicode_to_date(line[2]), line[0], (line[1])])
                 else:
-                    if line[2] in artist:
-                        listenings.append([unicode_to_date(line[4]), line[0], (line[2])])
+                    if line[1] in artist:
+                        listenings.append([unicode_to_date(line[2]), line[0], (line[1])])
             # handles Index Error caused by non utf-8 characters
             except IndexError:
                 continue
@@ -134,7 +142,7 @@ def read_data():
     return artists
 
 
-def select_artists(artists, n, threshold):
+def select_artists(artists, frequencies, n, threshold):
 
     # exclude artist with hotness < threshold
     new_artists = []
@@ -144,13 +152,23 @@ def select_artists(artists, n, threshold):
         else:
             new_artists.append(row)
 
-    # create a subset of n artist
+    # create n subsets of artists divided by hotness
     list = []
-    block = len(new_artists)/n
-    for count in range(0, len(new_artists), block):
-        if n > 0:
-            list.append(new_artists[count])
-        n -= 1
+    top = new_artists[0][1].hotness
+    block = (top - threshold)/n
+    j = 0
+
+    # foreach block take the artist with maximum frequencies
+    for i in range(0, n):
+        list.append(new_artists[j])
+        while (j < len(new_artists)) and (new_artists[j][1].hotness > top - block * (i + 1)):
+            try:
+                if frequencies[new_artists[j][0]] > frequencies[list[i][0]]:
+                    if new_artists[j][0] != '[unknown]':
+                        list[i] = new_artists[j]
+            except KeyError:
+                pass
+            j += 1
 
     return list
 
@@ -166,14 +184,17 @@ def into_dict(list):
 def main():
     # reads data from files
     artists = read_data()
+    users_info = ma_func.get_node_listening(artists)
+    artists_frequencies = ma_func.get_artists_frequencies(users_info)
 
     # sort artists for hotness value
     # transform dict in list
     artists = sorted(artists.items(), key=lambda x: x[1].hotness, reverse=True)
 
-    # select subset of n artist with minimum hotness
-    # selectArtists(list, n, minimum hotness)
-    subset = select_artists(artists, 5, 0.30)
+    # Select subset of _n_ artist and excludes those with a low hotness
+    # In each block of artist select who has the maximum frequency
+    # selectArtists(list, list_frequencies, _n_, minimum hotness)
+    subset = select_artists(artists, artists_frequencies, 5, 0.45)
 
     # transform subset into dictionary
     subset_dict = into_dict(subset)
@@ -181,19 +202,42 @@ def main():
     # get listeners list of an artist ordered by ascending date
     # TO-DO: ciclare su tutto subset_dict
     # TO-Do: salvare risultati in csv per grafici
-    listeners_list = get_listeners(subset_dict.items()[2])
+    for artist in subset_dict:
+        listeners_list = get_listeners(artist)
 
-    one_day = 86400
-    days_threshold = one_day * 8
-    # for each listeners_list build a tree
-    # where two users are connected if their listening
-    # is under the days_threshold
-    net = build_tree(listeners_list, days_threshold)
-    ts = nx.topological_sort(net)
-    # print(ts)
-    # print list(net.edges_iter(data=True))
-    # nx.draw_networkx(net, with_labels=True)
-    #  plt.savefig("data/path.png")
+        out_file = open('listening_analysis/local_diffusion/listening_' + artist + '.csv', 'w+')
+        for user in listeners_list:
+            print user
+            out_file.write(user[2].encode('utf-8') + "," + user[1].encode('utf-8') + "," + str(user[0]).encode('utf-8') + '\n')
+        out_file.close()
+
+        one_day = 86400
+        days = 5
+        days_threshold = one_day * days
+        # for each listeners_list build a tree
+        # where two users are connected if their listening
+        # is under the days_threshold
+        net = build_tree(listeners_list, days_threshold)
+        ts = nx.topological_sort(net)
+        print(ts)
+        print list(net.edges_iter(data=True))
+        # t = nx.dfs_tree(net, 0)
+        # print t.edges()
+        # nx.draw_networkx(net, with_labels=True)
+        pos = nx.spring_layout(net)  # positions for all nodes
+
+        # nodes
+        nx.draw_networkx_nodes(net, pos, node_size=700)
+
+        # edges
+        nx.draw_networkx_edges(net, pos, width=6)
+
+        # labels
+        nx.draw_networkx_labels(net, pos, font_size=20, font_family='sans-serif')
+
+        plt.axis('off')
+        plt.savefig("listening_analysis/local_diffusion/local_diffusion_" + str(days) + "_" + artist + ".png")
+        plt.show() # display
 
 if __name__ == '__main__':
     main()
